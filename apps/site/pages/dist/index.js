994f29f818831a9f76b3e40b5dffaf7f384cd83d
stanford <stanfordv@gmail.com>
2022-06-10 11:20:17 +0200

    tweak

--- apps/site/pages/dist/index.js
+++ apps/site/pages/dist/index.js
@@ -7,7 +7,7 @@ function Index() {
             React.createElement("h2", { className: "text-3xl font-mono tracking-tight text-gray-900 sm:text-4xl" },
                 React.createElement("span", { className: "block" }, "The Subroutine")),
             React.createElement("h3", { className: "text-2xl" },
-                React.createElement("span", { className: "block text-indigo-600 " }, "Post apocalyptic optimism")),
+                React.createElement("span", { className: "block text-indigo-600 " }, "Post apocalyptic optimism!")),
             React.createElement("div", { className: "mt-8 flex lg:mt-0 lg:flex-shrink-0" },
                 React.createElement("div", { className: "inline-flex rounded-md shadow" },
                     React.createElement("a", { href: "#", className: "inline-flex items-center justify-center px-5 py-3 border text-base font-medium rounded-md text-white bg-orange-500 hover:bg-black" }, "Soundcloud")),
